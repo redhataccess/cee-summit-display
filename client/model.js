415f30ce0ce70a6fdaebdd4f26b950c1b9ec2579
mwcz <mwc@clayto.com>
2016-05-19 13:59:36 -0400

    proper linking of newly added nodes

--- client/model.js
+++ client/model.js
@@ -1,25 +1,28 @@
 import Springy from 'springy';
 import noop from 'lodash/noop';
+import each from 'lodash/each';
+import filter from 'lodash/filter';
+import partial from 'lodash/partial';
 
 const graph = new Springy.Graph();
 
-const a1 = graph.newNode({ label: 'A1', group: 'a' });
-const a2 = graph.newNode({ label: 'A2', group: 'a' });
-const a3 = graph.newNode({ label: 'A3', group: 'a' });
-const a4 = graph.newNode({ label: 'A4', group: 'a' });
-const b1 = graph.newNode({ label: 'B1', group: 'b' });
-const b2 = graph.newNode({ label: 'B2', group: 'b' });
-const b3 = graph.newNode({ label: 'B3', group: 'b' });
+// const a1 = graph.newNode({ label: 'a1', group: 'a' });
+// const a2 = graph.newNode({ label: 'a2', group: 'a' });
+// const a3 = graph.newNode({ label: 'a3', group: 'a' });
+// const a4 = graph.newNode({ label: 'a4', group: 'a' });
+// const b1 = graph.newNode({ label: 'b1', group: 'b' });
+// const b2 = graph.newNode({ label: 'b2', group: 'b' });
+// const b3 = graph.newNode({ label: 'b3', group: 'b' });
 
-graph.newEdge(a1, a2);
-graph.newEdge(a1, a3);
-graph.newEdge(a1, a4);
-graph.newEdge(a2, a3);
-graph.newEdge(a2, a4);
-graph.newEdge(a3, a4);
+// graph.newEdge(a1, a2);
+// graph.newEdge(a1, a3);
+// graph.newEdge(a1, a4);
+// graph.newEdge(a2, a3);
+// graph.newEdge(a2, a4);
+// graph.newEdge(a3, a4);
 
-graph.newEdge(b1, b2);
-graph.newEdge(b2, b3);
+// graph.newEdge(b1, b2);
+// graph.newEdge(b2, b3);
 
 const layout = new Springy.Layout.ForceDirected(
     graph,
@@ -37,7 +40,21 @@ const renderer = new Springy.Renderer(
 
 renderer.start();
 
+function connect(node1, node2) {
+    graph.newEdge(node1, node2);
+}
+
+function createNode(node) {
+    const newNode = graph.newNode(node.data);
+    const friendNodes = filter(graph.nodes, { data: { group: node.data.group } });
+    console.log('new node', newNode);
+    console.log('friend nodes', friendNodes);
+    each(friendNodes, partial(connect, newNode));
+    return newNode;
+}
+
 function update(data) {
+    each(data, createNode);
 }
 
 function updateNode(func) {
