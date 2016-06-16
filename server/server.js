493c53e5a263eeafd67bff511dfc252c1fe5e0b9
mwcz <mwc@clayto.com>
2016-06-16 18:03:26 -0400

    send new ws clients all prior nodes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@
 
 const Hapi = require('hapi');
 const HapiWebSocket = require('hapi-plugin-websocket');
+const request = require('request');
 const server = new Hapi.Server();
+const _ = require('lodash');
 
 require('console-stamp')(console);
 
@@ -14,23 +16,21 @@ const INTERVAL = 50; // ms
 let wsServer;
 let lastId = 0;
 
-// TODO: before starting server, fetch all past state from Mongo and add it to
-// the initial buffer, so a restarted client app will be caught up with past
-// data.
-
-server.connection({ port: PORT });
+server.connection({
+    port: PORT,
+});
 server.register(HapiWebSocket, () => {
     server.route({
         method: 'GET',
         path: '/pending',
-        handler: (request, reply) => {
+        handler: (req, reply) => {
             reply(buffer);
         },
     });
     server.route({
         method: 'OPTIONS',
         path: '/record',
-        handler: (request, reply) => {
+        handler: (req, reply) => {
             reply()
             .header('Access-Control-Allow-Origin', '*')
             .header('Access-Control-Allow-Headers', 'charset, content-type')
@@ -40,9 +40,9 @@ server.register(HapiWebSocket, () => {
     server.route({
         method: 'POST',
         path: '/record',
-        handler: (request, reply) => {
-            console.log('POST /record received');
-            buffer.push({ id: ++lastId, data: request.payload });
+        handler: (req, reply) => {
+            console.log('record received');
+            buffer.push({ id: ++lastId, data: req.payload });
             reply('recorded')
                 .header('Access-Control-Allow-Origin', '*')
                 .header('Access-Control-Allow-Headers', 'charset, content-type')
@@ -58,6 +58,9 @@ server.register(HapiWebSocket, () => {
                     only: true,
                     connect: (wss, ws) => {
                         console.log('client connected');
+                        // send any attendees registered before display
+                        // launched
+                        fetchAttendees(ws.send.bind(ws));
                         wsServer = wss;
                     },
                     disconnect: (wss, ws) => {
@@ -65,7 +68,7 @@ server.register(HapiWebSocket, () => {
                 },
             },
         },
-        handler: (request, reply) => {},
+        handler: (req, reply) => {},
     });
     server.start(err => {
         if (err) {
@@ -75,11 +78,37 @@ server.register(HapiWebSocket, () => {
     });
 });
 
+function transformAttendees(attendees) {
+    return _.chain(attendees)
+        .map(attendee => _.pick(attendee, ['challenge', 'color']))
+        .map(attendee => {
+            return {
+                id: ++lastId,
+                data: { color: attendee.color, group: attendee.challenge },
+            };
+        })
+        .value();
+}
+
+function fetchAttendees(callback) {
+    request('http://localhost:3000/attendees', (error, response, attendeesJSON) => {
+        try {
+            const attendees = JSON.parse(attendeesJSON);
+            const xformed = transformAttendees(attendees);
+            callback(JSON.stringify(xformed));
+        }
+        catch (e) {
+            console.error(e);
+        }
+    });
+}
+
 function broadcast(wss, data) {
     wss.clients.forEach(client => {
         try {
             client.send(data);
-        } catch (e) {
+        }
+        catch (e) {
             console.error('tried to send websocket message to closed client; ignoring.');
         }
     });
